c6b89907b5355e25a0b9dea04d727f12f92720b2
davidbyoung <dbyoung2@gmail.com>
2019-08-24 23:43:39 -0500

    Updated to use Travis build number instead of ID, bash errors now cause an immediate exit

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,5 +1,7 @@
+set -e
+
 SOURCE_DIR="$1"
-BUILD_ID="$2"
+BUILD_NUMBER="$2"
 SSH_USER="$3"
 SSH_HOST="$4"
 
@@ -9,9 +11,9 @@ then
     exit 1
 fi
 
-if [ -z "$BUILD_ID" ]
+if [ -z "$BUILD_NUMBER" ]
 then
-    echo "No build ID specified"
+    echo "No build number specified"
     exit 1
 fi
 
@@ -28,12 +30,13 @@ then
 fi
 
 html_dir=/var/www/html
-target_dir=$html_dir/releases/$BUILD_ID
+target_dir=$html_dir/releases/$BUILD_NUMBER
 echo "rsync'ing to host"
+# Purposely leaving trailing slash on the source directory so that the contents get copied
 rsync -aq --delete-after --rsync-path="mkdir -p $target_dir/ && rsync" "$SOURCE_DIR" $SSH_USER@$SSH_HOST:$target_dir/
 
 echo "Creating symlinks and swapping"
-ssh $SSH_USER@$SSH_HOST <<EOF
+ssh $SSH_USER@$SSH_HOST /bin/bash <<EOF
 ln -snf $(readlink $html_dir/current) $html_dir/previous
 ln -snf $target_dir $html_dir/current
 EOF
