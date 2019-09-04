0e0acfb0b85abd67c5644992196e6be6ad7e61dd
davidbyoung <dbyoung2@gmail.com>
2019-09-03 23:52:36 -0500

    Implemented fully-functional mobile styling

--- public-web/js/scripts.js
+++ public-web/js/scripts.js
@@ -1,8 +1,25 @@
 window.addEventListener('load', loadEvent => {
     // Add focus to the search bar as long as there's no hash (autofocus attribute prevents scrolling to the ID held in the hash)
-    if(!window.location.hash)
-    {
+    if (!window.location.hash) {
         document.getElementById('search-query').focus();
+    }
+
+    // Handle toggling the mobile menu
+    document.querySelector('#mobile-menu a').onclick = () => {
+        if (mobileMenu.isOpen()) {
+            mobileMenu.close();
+        } else {
+            mobileMenu.open();
+        }
+
+        return false;
+    };
+
+    // Close the sidebar when clicking on gray-out
+    if (document.getElementById('gray-out')) {
+        document.getElementById('gray-out').onclick = () => {
+            mobileMenu.close();
+        };
     }
 
     const searchInputElem = document.getElementById('search-query');
@@ -15,18 +32,14 @@ window.addEventListener('load', loadEvent => {
     };
     let timer = null;
     searchInputElem.onkeyup = keyEvent => {
-        if(timer !== null)
-        {
+        if (timer !== null) {
             clearTimeout(timer);
         }
 
-        if(searchInputElem.value.length === 0)
-        {
+        if (searchInputElem.value.length === 0) {
             searchResultsElem.style.display = 'none';
             document.removeEventListener('click', detectClickOffSearch);
-        }
-        else
-        {
+        } else {
             timer = setTimeout(() => {
                 fetch(`${apiUri}/docs/search?query=${encodeURIComponent(searchInputElem.value)}`)
                     .then((response) => response.json())
@@ -37,30 +50,25 @@ window.addEventListener('load', loadEvent => {
                             let linkInnerHtml = '';
 
                             // In the case of a match on the doc itself, don't bother showing the context
-                            if(searchResult.htmlElementType !== 'h1')
-                            {
+                            if (searchResult.htmlElementType !== 'h1') {
                                 linkInnerHtml = '<span class="search-result-context">';
                                 // The <h1> is always assumed to be set
                                 linkInnerHtml += `<h1>${searchResult.highlightedH1}</h1>`;
 
                                 // Only show the header context if the result is not the level of header
-                                if(searchResult.htmlElementType !== 'h2' && searchResult.highlightedH2 !== null)
-                                {
+                                if (searchResult.htmlElementType !== 'h2' && searchResult.highlightedH2 !== null) {
                                     linkInnerHtml += ` > <h2>${searchResult.highlightedH2}</h2>`
                                 }
 
-                                if(searchResult.htmlElementType !== 'h3' && searchResult.highlightedH3 !== null)
-                                {
+                                if (searchResult.htmlElementType !== 'h3' && searchResult.highlightedH3 !== null) {
                                     linkInnerHtml += ` > <h3>${searchResult.highlightedH3}</h3>`
                                 }
 
-                                if(searchResult.htmlElementType !== 'h4' && searchResult.highlightedH4 !== null)
-                                {
+                                if (searchResult.htmlElementType !== 'h4' && searchResult.highlightedH4 !== null) {
                                     linkInnerHtml += ` > <h4>${searchResult.highlightedH4}</h4>`
                                 }
 
-                                if(searchResult.htmlElementType !== 'h5' && searchResult.highlightedH5 !== null)
-                                {
+                                if (searchResult.htmlElementType !== 'h5' && searchResult.highlightedH5 !== null) {
                                     linkInnerHtml += ` > <h5>${searchResult.highlightedH5}</h5>`
                                 }
 
@@ -71,8 +79,7 @@ window.addEventListener('load', loadEvent => {
                             searchResultItems += `<li><a href="${searchResult.link}" title="View this result">${linkInnerHtml}</a></li>`
                         });
 
-                        if(searchResultItems === '')
-                        {
+                        if (searchResultItems === '') {
                             // No results
                             searchResultItems = `<li class="no-results">No results for "${searchInputElem.value}"</li>`;
                         }
@@ -88,3 +95,15 @@ window.addEventListener('load', loadEvent => {
         }
     };
 });
+
+const mobileMenu = {
+    isOpen: () => {
+        return document.body.classList.contains('nav-open');
+    },
+    close: () => {
+        document.body.classList.remove('nav-open');
+    },
+    open: () => {
+        document.body.classList.add('nav-open');
+    }
+};
