8a78f9834ff0aeeccb7cf3d1d690281125786617
davidbyoung <dbyoung2@gmail.com>
2019-09-07 00:00:29 -0500

    Added fallback to $_SERVER to read environment vars that are set by a CI server

--- src/Web/Bootstrappers/ViewCompilerBootstrapper.php
+++ src/Web/Bootstrappers/ViewCompilerBootstrapper.php
@@ -31,7 +31,8 @@ final class ViewCompilerBootstrapper extends Bootstrapper
             __DIR__ . '/../../../resources/views',
             __DIR__ . '/../../../public-web',
             $container->resolve(DocumentationMetadata::class),
-            $_ENV['APP_API_URL']
+            // $_ENV does not get populated from a CI server
+            $_ENV['APP_API_URL'] ?? $_SERVER['APP_URI_URL']
         );
         $container->bindInstance(ViewCompiler::class, $viewCompiler);
     }
