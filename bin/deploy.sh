cab7b2898524ac583e399faddcd655bc95eb267c
davidbyoung <dbyoung2@gmail.com>
2019-09-09 20:49:17 -0500

    Fixed build script so that the resources and tmp directories are temporarily owned by the ci user

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -38,11 +38,13 @@ rsync -aq --delete-after --rsync-path="mkdir -p $target_dir/ && rsync" "$SOURCE_
 echo "Creating symlinks and swapping"
 ssh $SSH_USER@$SSH_HOST /bin/bash <<EOF
 cp -vrf $html_dir/.env $target_dir/.env
-sudo chown -R 48:48 $target_dir/resources
-sudo chown -R 48:48 $target_dir/tmp
+sudo chown -R 1000:48 $target_dir/resources
+sudo chown -R 1000:48 $target_dir/tmp
 sudo chmod -R 755 $target_dir/resources
 sudo chmod -R 755 $target_dir/tmp
 php $target_dir/aphiria docs:index
+sudo chown -R 48:48 $target_dir/resources
+sudo chown -R 48:48 $target_dir/tmp
 ln -snf $(readlink $html_dir/current) $html_dir/previous
 ln -snf $target_dir $html_dir/current
 EOF
