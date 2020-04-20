bb014cf7722bd24e483213e0d8f3d9a15f70bb8a
davidbyoung <dbyoung2@gmail.com>
2020-04-20 18:30:32 -0500

    Added Postgres service to Travis CI config so that a temporary DB is started when the bootstrapper metadata collector us running

--- src/Databases/Binders/SqlBinder.php
+++ src/Databases/Binders/SqlBinder.php
@@ -15,6 +15,8 @@ namespace App\Databases\Binders;
 use Aphiria\DependencyInjection\Binders\Binder;
 use Aphiria\DependencyInjection\IContainer;
 use PDO;
+use PDOException;
+use RuntimeException;
 
 /**
  * Defines the SQL binder
@@ -26,13 +28,17 @@ final class SqlBinder extends Binder
      */
     public function bind(IContainer $container): void
     {
-        $dsn = sprintf(
-            'pgsql:host=%s;dbname=%s;port=%d;options=\'--client_encoding=utf8\'',
-            \getenv('DB_HOST'),
-            \getenv('DB_NAME'),
-            (int)\getenv('DB_PORT')
-        );
-        $pdo = new PDO($dsn, \getenv('DB_USER'), \getenv('DB_PASSWORD'));
-        $container->bindInstance(PDO::class, $pdo);
+        try {
+            $dsn = sprintf(
+                'pgsql:host=%s;dbname=%s;port=%d;options=\'--client_encoding=utf8\'',
+                \getenv('DB_HOST'),
+                \getenv('DB_NAME'),
+                (int)\getenv('DB_PORT')
+            );
+            $pdo = new PDO($dsn, \getenv('DB_USER'), \getenv('DB_PASSWORD'));
+            $container->bindInstance(PDO::class, $pdo);
+        } catch (PDOException $ex) {
+            throw new RuntimeException('Failed to connect to database', 0, $ex);
+        }
     }
 }
