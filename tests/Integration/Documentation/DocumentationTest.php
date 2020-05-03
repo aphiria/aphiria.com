6ada92b533095e2aa2e9b2796ba109b5332ab1f4
davidbyoung <dbyoung2@gmail.com>
2020-05-02 19:12:30 -0500

    Updated composer dependencies, added integration tests

---
+++ tests/Integration/Documentation/DocumentationTest.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Aphiria
+ *
+ * @link      https://www.aphiria.com
+ * @copyright Copyright (C) 2020 David Young
+ * @license   https://github.com/aphiria/aphiria.com/blob/master/LICENSE.md
+ */
+
+declare(strict_types=1);
+
+namespace App\Tests\Integration\Documentation;
+
+use App\Documentation\Searching\SearchResult;
+use App\Tests\Integration\IntegrationTestCase;
+
+class DocumentationTest extends IntegrationTestCase
+{
+    public function testSearchingForItemWithDocumentationReturnsResults(): void
+    {
+        $response = $this->get('http://localhost/docs/search?query=routing');
+        $this->assertParsedBodyPassesCallback(
+            $response,
+            SearchResult::class . '[]',
+            static function (array $results) {
+                return \count($results) > 0 && strpos($results[0]->highlightedH1, 'Routing') !== false;
+            }
+        );
+    }
+}
