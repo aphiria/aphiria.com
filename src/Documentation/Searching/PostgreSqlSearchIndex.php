d239e608713e8485076eb9e952916cd4a73b6be2
David Young <dbyoung2@gmail.com>
2021-10-10 20:23:54 -0500

    Added more debugging info

--- src/Documentation/Searching/PostgreSqlSearchIndex.php
+++ src/Documentation/Searching/PostgreSqlSearchIndex.php
@@ -69,7 +69,7 @@ final class PostgreSqlSearchIndex implements ISearchIndex
             foreach ($htmlPaths as $htmlPath) {
                 \libxml_use_internal_errors(true);
                 if ($dom->loadHTML((string)$this->files->read($htmlPath)) === false) {
-                    throw new Exception('Failed to load HTML: ' . \libxml_get_last_error()->message);
+                    throw new Exception("Failed to load HTML for $htmlPath: " . \libxml_get_last_error()->message);
                 }
 
                 \libxml_clear_errors();
