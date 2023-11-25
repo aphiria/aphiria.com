9796cb33be1c2d473ee510e90a0bbb4a772974bb
davidbyoung <dbyoung2@gmail.com>
2023-11-25 17:29:37 -0600

    Fixed import ID of S3 bucket

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -21,7 +21,7 @@ provider "kubernetes" {
 # We likely had to provision the S3 bucket manually for it to store state (chicken-and-the-egg problem).  So, make sure we import it into state after the fact.Z
 import {
     to = digitalocean_spaces_bucket.aphiria_com_infrastructure
-    id = "aphiria-com-infrastructure"
+    id = "nyc3,aphiria-com-infrastructure"
 }
 
 resource "digitalocean_spaces_bucket" "aphiria_com_infrastructure" {
