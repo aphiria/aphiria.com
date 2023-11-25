389d487d9ce05b91cf13b68ca9a94ce298f2a41f
davidbyoung <dbyoung2@gmail.com>
2023-11-25 10:12:24 -0600

    Fixed typo

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -30,7 +30,7 @@ resource "digitalocean_record" "a" {
     type = "A"
     name = "@"
     # value = "159.89.187.188"
-    value = data.kubernetes_service.load_balancer.load_balancer_ingress[0].ip
+    value = data.kubernetes_service.load_balancer.spec.load_balancer_ip
     ttl = 3600
 }
 
@@ -39,7 +39,7 @@ resource "digitalocean_record" "api_a" {
     type = "A"
     name = "api"
     # value = "159.89.187.188"
-    value = data.kubernetes_service.load_balancer.load_balancer_ingress[0].ip
+    value = data.kubernetes_service.load_balancer.spec.load_balancer_ip
     ttl = 3600
 }
 
