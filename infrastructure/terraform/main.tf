e4fc1077e812f7167f08191844b916405cb82312
davidbyoung <dbyoung2@gmail.com>
2023-11-25 17:22:05 -0600

    Added S3 bucket to state

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -17,3 +17,15 @@ provider "kubernetes" {
     token = module.kubernetes.cluster_kube_config_token
     cluster_ca_certificate = base64decode(module.kubernetes.cluster_ca_certificate)
 }
+
+# We likely had to provision the S3 bucket manually for it to store state (chicken-and-the-egg problem).  So, make sure we import it into state after the fact.
+import {
+    provider = digitalocean_spaces_bucket
+    to = digitalocean_spaces_bucket.aphiria_com_infrastructure
+    id = "aphiria-com-infrastructure"
+}
+
+resource "digitalocean_spaces_bucket" "aphiria_com_infrastructure" {
+    name = "aphiria-com-infrastructure"
+    region = "nyc3"
+}
