93773f446112ca0008e868d6338f970b390c31a6
davidbyoung <dbyoung2@gmail.com>
2024-10-27 16:28:02 -0500

    Updated CD to use same install.sh script as running locally

--- install.sh
+++ install.sh
@@ -1,34 +1,83 @@
-# NOTE: Keep these version in sync with the ones used by GitHub Actions
+#!/bin/bash
+
+# Default values
+install_kubectl=false
+install_minikube=false
+install_helm=false
+install_helmfile=false
+install_terraform=false
+
+# Parse arguments
+for arg in "$@"; do
+    case $arg in
+        --install-kubectl)
+            install_kubectl=true
+            ;;
+        --install-minikube)
+            install_minikube=true
+            ;;
+        --install-helm)
+            install_helm=true
+            ;;
+        --install-helmfile)
+            install_helmfile=true
+            ;;
+        --install-terraform)
+            install_terraform=true
+            ;;
+        *)
+            # If any other argument is passed, do nothing
+            ;;
+    esac
+done
+
+# Check if no arguments are passed, which we'll interpret as "install everything"
+if [ $# -eq 0 ]; then
+    install_kubectl=true
+    install_minikube=true
+    install_helm=true
+    install_helmfile=true
+    install_terraform=true
+fi
+
+original_dir=$(pwd)
 cd /tmp
 
-# Install Kubectl (https://kubernetes.io/docs/tasks/tools)
-echo "Installing Kubectl"
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-kubectl version
+if [ "$install_kubectl" = true ]; then
+    echo "Installing Kubectl (https://kubernetes.io/docs/tasks/tools)"
+    curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+    sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+    kubectl version
+fi
 
-# Install Minikube (https://minikube.sigs.k8s.io/docs/start/)
-echo "Installing Minikube"
-curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
-minikube version
+if [ "$install_minikube" = true ]; then
+    echo "Installing Minikube (https://minikube.sigs.k8s.io/docs/start/)"
+    curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+    sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
+    minikube version
+fi
 
-# Install Helm (https://helm.sh/docs/intro/install/)
-echo "Installing Helm"
-curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
-chmod 700 get_helm.sh
-./get_helm.sh
-helm version
+if [ "$install_helm" = true ]; then
+    echo "Installing Helm (https://helm.sh/docs/intro/install/)"
+    curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+    chmod 700 get_helm.sh
+    ./get_helm.sh
+    helm version
+fi
 
-# Install Helmfile (https://helmfile.readthedocs.io/en/latest/#installation)
-echo "Installing Helmfile"
-curl -L https://github.com/helmfile/helmfile/releases/download/v1.0.0-rc.7/helmfile_1.0.0-rc.7_linux_amd64.tar.gz -o helmfile.tar.gz
-tar -xzf helmfile.tar.gz
-sudo mv helmfile /usr/local/bin/helmfile
-helmfile --version
+if [ "$install_helmfile" = true ]; then
+    echo "Installing Helmfile (https://helmfile.readthedocs.io/en/latest/#installation)"
+    curl -L https://github.com/helmfile/helmfile/releases/download/v1.0.0-rc.7/helmfile_1.0.0-rc.7_linux_amd64.tar.gz -o helmfile.tar.gz
+    tar -xzf helmfile.tar.gz
+    sudo mv helmfile /usr/local/bin/helmfile
+    helmfile --version
+fi
 
-# Install Terraform (https://developer.hashicorp.com/terraform/install)
-echo "Installing Terraform"
-curl -lO https://releases.hashicorp.com/terraform/1.9.8/terraform_1.9.8_linux_amd64.zip
-sudo unzip terraform_1.9.8_linux_amd64.zip -d /usr/local/bin
-terraform --version
+if [ "$install_terraform" = true ]; then
+    echo "Installing Terraform (https://developer.hashicorp.com/terraform/install)"
+    curl -lO https://releases.hashicorp.com/terraform/1.9.8/terraform_1.9.8_linux_amd64.zip
+    sudo unzip terraform_1.9.8_linux_amd64.zip -d /usr/local/bin
+    terraform --version
+fi
+
+cd "$original_dir"
