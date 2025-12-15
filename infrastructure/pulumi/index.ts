1a73213f29d7397de4cf63d81545cbe69785a787
David Young <dbyoung2@gmail.com>
2025-12-14 22:53:58 -0600

    Pulumi cleanup (#95)
    * Started cleaning up Pulumi codebase
    
    * Added linting and formatting of TypeScript
    
    * Rename some CI jobs for readability
    
    * Cleaned up another CI job name
    
    * Removed unnecessary flags

--- infrastructure/pulumi/index.ts
+++ infrastructure/pulumi/index.ts
@@ -1,11 +1,11 @@
 import * as kubernetes from "@pulumi/kubernetes";
-import { createKubernetesCluster } from "./src/kubernetes";
-import { createMonitoring } from "./src/monitoring";
-import { createNetworking } from "./src/networking";
-import { createStorage } from "./src/storage";
+import { createKubernetesCluster } from "./kubernetes";
+import { createMonitoring } from "./monitoring";
+import { createNetworking } from "./networking";
+import { createStorage } from "./storage";
 
 // Create storage bucket (for state and other infrastructure needs)
-const storage = createStorage();
+createStorage();
 
 // Create Kubernetes cluster
 const k8s = createKubernetesCluster();
@@ -16,10 +16,10 @@ const kubeProvider = new kubernetes.Provider("kubernetes-provider", {
 });
 
 // Create monitoring (depends on cluster being available)
-const monitoring = createMonitoring();
+createMonitoring();
 
 // Create networking (depends on cluster and load balancer being available)
-const networking = createNetworking(kubeProvider);
+createNetworking(kubeProvider);
 
 // Export outputs that are needed by other tooling
 export const clusterId = k8s.clusterId;
