eda8893780e985a2c03dcc3e13b941db8f2601fc
davidbyoung <dbyoung2@gmail.com>
2025-12-14 20:37:06 -0600

    Fixed various Pulumi errors during initial import from TF

--- infrastructure/pulumi/src/monitoring.ts
+++ infrastructure/pulumi/src/monitoring.ts
@@ -2,17 +2,23 @@ import * as digitalocean from "@pulumi/digitalocean";
 
 export function createMonitoring() {
     const apiUptimeCheck = new digitalocean.UptimeCheck("api", {
+        enabled: true,
         name: "API Health Check",
         type: "https",
         target: "https://api.aphiria.com/health",
         regions: ["us_east", "us_west"],
+    }, {
+        protect: true,
     });
 
     const webUptimeCheck = new digitalocean.UptimeCheck("web", {
+        enabled: true,
         name: "Web Health Check",
         type: "https",
         target: "https://www.aphiria.com",
         regions: ["us_east", "us_west"],
+    }, {
+        protect: true,
     });
 
     return {
--- infrastructure/pulumi/src/storage.ts
+++ infrastructure/pulumi/src/storage.ts
@@ -4,6 +4,9 @@ export function createStorage() {
     const bucket = new digitalocean.SpacesBucket("aphiria-com-infrastructure", {
         name: "aphiria-com-infrastructure",
         region: "nyc3",
+        acl: "private",
+    }, {
+        protect: true,
     });
 
     return {
